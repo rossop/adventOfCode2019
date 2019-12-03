e26eab43759f19c3d69abc84bdd5cc8a9f044e85
Rosso <peter.rosso@bristol.ac.uk>
2019-12-03 22:17:39 +0000

    Works, but not sure why i need to import the values in the for/for loops. [Some issues with list assignments]
    Signed-off-by: Rosso <peter.rosso@bristol.ac.uk>

--- day2.py
+++ day2.py
@@ -1,15 +1,13 @@
-import re
+# with open('day2_input.txt') as f:
+#     instructionsList = f.read()
+#
+# instructionsList = instructionsList.rstrip('\n').split(',')
+# instructionsList = [int(i) for i in instructionsList]
 
 with open('day2_input.txt') as f:
-    instructionsList = f.read()
+    insList = list(map(int, f.read().rstrip('\n').split(",")))
 
-instructionsList = instructionsList.rstrip('\n').split(',')
-instructionsList = [int(i) for i in instructionsList]
-
-
-
-def Intcode (instructionsList, noun = 12, verb = 2):
-
+def intcode (instructionsList, noun = 12, verb = 2):
     """ Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values used immediately after an opcode,
     if any, are called the instruction's parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3,
     and 4 are the parameters. The instruction 99 contains only an opcode and has no parameters
@@ -41,16 +39,22 @@ def Intcode (instructionsList, noun = 12, verb = 2):
 
     return instructionsList
 
+instructions = intcode(insList)
+print('Day2:\n{}'.format(str(instructions[0])))
 
-print('Day2:\n{}\n{}'.format(str(instructions),str(predictions)))
-invIntcode(instructionsList)
 
-for ii = range(100):
-    for jj = range(100):
-        instructions = Intcode(instructionsList, ii, jj)
-            if instrunctions[0] == 19690720:
+for ii in range(100):
+    for jj in range(100):
+        try:
+            #TODO why do i have to import here?
+            with open('day2_input.txt') as f:
+                insList = list(map(int, f.read().rstrip('\n').split(",")))
+            instructions = intcode(insList, ii, jj)
+            if instructions[0] == 19690720:
                 print(str(ii)+str(jj))
                 break
+        except:
+            continue
 
 
 
